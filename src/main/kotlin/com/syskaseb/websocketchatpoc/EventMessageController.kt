946bc2d15c378dbd796d8b4fe7e873d85dc177cc
Sebastian Syska <syska.seb@gmail.com>
2021-04-03 09:39:14 +0200

    ktlint formatting

--- src/main/kotlin/com/syskaseb/websocketchatpoc/EventMessageController.kt
+++ src/main/kotlin/com/syskaseb/websocketchatpoc/EventMessageController.kt
@@ -1,9 +1,5 @@
 package com.syskaseb.websocketchatpoc
 
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.stereotype.Controller
-
 /*
 @Controller
 class EventMessageController {
--- src/main/kotlin/com/syskaseb/websocketchatpoc/WebSocketConfig.kt
+++ src/main/kotlin/com/syskaseb/websocketchatpoc/WebSocketConfig.kt
@@ -7,12 +7,11 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
-
 @Configuration
 @EnableWebSocket
 class WebSocketConfig : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(myHandler(), "/games/tictactoe").setAllowedOrigins("*");
+        registry.addHandler(myHandler(), "/games/tictactoe").setAllowedOrigins("*")
     }
 
     @Bean
